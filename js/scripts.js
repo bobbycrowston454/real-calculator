5be5510b5ab47c43991060d361bfd4431cddfb24
Bobby Crowston and Seth Kendall <seth@>
2016-08-10 13:18:33 -0700

    update with forms for other functions

--- js/scripts.js
+++ js/scripts.js
@@ -16,40 +16,41 @@ var divide = function(number1, number2) {
 
 $(document).ready(function() {
   $("form#add").submit(function(event) {
-    event.preventDefault();
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
     var result = add(number1, number2);
+
     $("#output").text(result);
+    event.preventDefault();
   });
 });
 
 $(document).ready(function() {
   $("form#subtract").submit(function(event) {
-    event.preventDefault();
     var number1 = parseInt($("#subtract1").val());
     var number2 = parseInt($("#subtract2").val());
     var result = subtract(number1, number2);
     $("#output").text(result);
+    event.preventDefault();
   });
 });
-
+debugger;
 $(document).ready(function() {
   $("form#multiply").submit(function(event) {
     event.preventDefault();
     var number1 = parseInt($("#multiply1").val());
-    var number2 = parseInt($("#mulitply2").val());
-    var result = add(number1, number2);
+    var number2 = parseInt($("#multiply2").val());
+    var result = multiply(number1, number2);
     $("#output").text(result);
   });
 });
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("form#divide").submit(function(event) {
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var result = add(number1, number2);
+    var number1 = parseInt($("#divide1").val());
+    var number2 = parseInt($("#divide2").val());
+    var result = divide(number1, number2);
     $("#output").text(result);
   });
 });
